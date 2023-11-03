b4012f37f9f6afcd51e35d72ec97e823607af258
Priscila Padilla Latance <priscila.latance@gmail.com>
2023-11-03 13:21:56 -0300

    Projeto com login Funcional

--- controleFrota/src/views/telaLogin.java
+++ controleFrota/src/views/telaLogin.java
@@ -39,73 +39,90 @@ public class telaLogin extends javax.swing.JFrame {
         usuario = new app.bolivia.swing.JCTextField();
         senha = new jpass.JRPasswordField();
         btnLogin = new javax.swing.JButton();
-        cadastroUsuario = new javax.swing.JButton();
+        btnRessetSenha = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
         telaInicial = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Abraão Lemos LTDA");
-        getContentPane().setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
+        setLocationByPlatform(true);
+        setMaximumSize(null);
+        setPreferredSize(new java.awt.Dimension(1390, 900));
+        setResizable(false);
+        java.awt.FlowLayout flowLayout1 = new java.awt.FlowLayout(java.awt.FlowLayout.LEFT);
+        flowLayout1.setAlignOnBaseline(true);
+        getContentPane().setLayout(flowLayout1);
 
-        painelLogin.setPreferredSize(new java.awt.Dimension(1600, 1000));
+        painelLogin.setAutoscrolls(true);
+        painelLogin.setMaximumSize(new java.awt.Dimension(0, 0));
+        painelLogin.setMinimumSize(new java.awt.Dimension(0, 0));
+        painelLogin.setPreferredSize(new java.awt.Dimension(1390, 900));
+        painelLogin.setRequestFocusEnabled(false);
         painelLogin.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        usuario.setBackground(new java.awt.Color(34, 102, 145));
+        usuario.setBackground(java.awt.SystemColor.controlHighlight);
         usuario.setBorder(null);
-        usuario.setForeground(new java.awt.Color(255, 255, 255));
-        usuario.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        usuario.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         usuario.setOpaque(false);
-        usuario.setPhColor(new java.awt.Color(255, 255, 255));
-        usuario.setPlaceholder("USUARIO");
+        usuario.setSelectedTextColor(new java.awt.Color(0, 0, 0));
         usuario.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyReleased(java.awt.event.KeyEvent evt) {
                 usuarioKeyReleased(evt);
             }
         });
-        painelLogin.add(usuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(830, 420, 350, -1));
+        painelLogin.add(usuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(850, 360, 310, -1));
 
-        senha.setBackground(new java.awt.Color(34, 102, 145));
+        senha.setBackground(java.awt.SystemColor.controlHighlight);
         senha.setBorder(null);
-        senha.setForeground(new java.awt.Color(255, 255, 255));
-        senha.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        senha.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         senha.setName(""); // NOI18N
         senha.setOpaque(false);
         senha.setPhColor(new java.awt.Color(255, 255, 255));
-        senha.setPlaceholder("SENHA");
-        senha.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                senhaActionPerformed(evt);
-            }
-        });
-        painelLogin.add(senha, new org.netbeans.lib.awtextra.AbsoluteConstraints(830, 518, 350, -1));
+        painelLogin.add(senha, new org.netbeans.lib.awtextra.AbsoluteConstraints(850, 450, 310, -1));
 
-        btnLogin.setToolTipText("");
+        btnLogin.setContentAreaFilled(false);
+        btnLogin.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btnLogin.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnLoginActionPerformed(evt);
             }
         });
-        painelLogin.add(btnLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(920, 625, 160, 30));
-        painelLogin.add(cadastroUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 340, 220, 30));
+        painelLogin.add(btnLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(930, 540, 140, 30));
 
+        btnRessetSenha.setContentAreaFilled(false);
+        btnRessetSenha.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnRessetSenha.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRessetSenhaActionPerformed(evt);
+            }
+        });
+        painelLogin.add(btnRessetSenha, new org.netbeans.lib.awtextra.AbsoluteConstraints(920, 690, 160, 20));
+
+        jPanel1.setBackground(new java.awt.Color(48, 105, 139));
+        painelLogin.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 280, 220, 50));
+
+        telaInicial.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        telaInicial.setForeground(java.awt.Color.white);
+        telaInicial.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagem/Tela Login.png"))); // NOI18N
         telaInicial.setToolTipText("");
-        telaInicial.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
-        painelLogin.add(telaInicial, new org.netbeans.lib.awtextra.AbsoluteConstraints(-140, -30, 1561, -1));
+        telaInicial.setMaximumSize(new java.awt.Dimension(0, 0));
+        telaInicial.setMinimumSize(new java.awt.Dimension(0, 0));
+        telaInicial.setName(""); // NOI18N
+        telaInicial.setVerifyInputWhenFocusTarget(false);
+        painelLogin.add(telaInicial, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, -30, 1450, 873));
 
         getContentPane().add(painelLogin);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    
+    
     private void usuarioKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_usuarioKeyReleased
-        usuario.setText(usuario.getText().toUpperCase());
+       usuario.setText(usuario.getText().toLowerCase());
     }//GEN-LAST:event_usuarioKeyReleased
 
-    private void senhaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_senhaActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_senhaActionPerformed
-
     private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginActionPerformed
-
         String user = usuario.getText();        
         String password = senha.getText();
         
@@ -113,15 +130,16 @@ public class telaLogin extends javax.swing.JFrame {
         if(user.equals("") || password.equals("")){
             //Retorna uma tela com a mensagem de erro
             JOptionPane.showMessageDialog(this, "Preencha os Campos", "Login", 0,
-                    new ImageIcon(getClass().getResource("/imagens/info.png")));
+                    new ImageIcon(getClass().getResource("/Imagem/info.png")));
             
         }else{
             //chama o método que faz o login
            Logar(user, password);
-        } 
+        }               
     }//GEN-LAST:event_btnLoginActionPerformed
 
-        //Criando a conexão com o banco
+    
+    //Criando a conexão com o banco
     Conexao con = new Conexao();
     Connection cn = con.conexao();
     
@@ -143,7 +161,7 @@ public class telaLogin extends javax.swing.JFrame {
                 Statement st1 = cn.createStatement();
                 ResultSet rs1 = st1.executeQuery(sql1);
                 
-                if(rs1.first()){
+                if(rs1.next()){
                     /*//Define o tipo de Usuario, Adm ou padrão
                     String sql2 = "SELECT tipo_us FROM usuario WHERE email = '" + id + "' "
                             + "and senha = '" + senha + "' ";
@@ -159,9 +177,11 @@ public class telaLogin extends javax.swing.JFrame {
                     //Fecha a janela atual(Login)
                     dispose();
                     //Chama a tela de Menu
-                    //MenuPrincipal menu = new MenuPrincipal();
-                    JOptionPane.showMessageDialog(this, "Bem vindo ao Sistema " + dado, "Usuario", 0,
-                        new ImageIcon(getClass().getResource("/imagens/info.png")));                    
+                    telaInicial menu = new telaInicial();
+                    menu.setLocationRelativeTo(null);
+                    menu.setVisible(true);
+                    //JOptionPane.showMessageDialog(this, "Bem vindo ao Sistema " + dado, "Usuario", 0,
+                       // new ImageIcon(getClass().getResource("/imagens/info.png")));                    
                     
                     //Caso o perfil seja Administrador
                     /*if (dado.equals("ADM")){
@@ -213,25 +233,27 @@ public class telaLogin extends javax.swing.JFrame {
                     
                 }else{
                      JOptionPane.showMessageDialog(this, "Senha Incorreta!", "Login", 0,
-                    new ImageIcon(getClass().getResource("/imagens/info.png")));
+                    new ImageIcon(getClass().getResource("/Imagem/info.png")));
                 }
                 
             }else{
                 JOptionPane.showMessageDialog(this, "O usuário não existe no banco de dados!", "Login", 0,
-                    new ImageIcon(getClass().getResource("/imagens/info.png")));
+                    new ImageIcon(getClass().getResource("/Imagem/info.png")));
             }
                       
         } catch (Exception e) {
         }
     
-    }
+    }    
+    
+    private void btnRessetSenhaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRessetSenhaActionPerformed
+          
+    }//GEN-LAST:event_btnRessetSenhaActionPerformed
 
+    
     /**
-     * This method is called from within the constructor to initialize the form.
-     * WARNING: Do NOT modify this code. The content of this method is always
-     * regenerated by the Form Editor.
-     */                                                
-
+     * @param args the command line arguments
+     */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -256,6 +278,12 @@ public class telaLogin extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -269,7 +297,8 @@ public class telaLogin extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnLogin;
-    private javax.swing.JButton cadastroUsuario;
+    private javax.swing.JButton btnRessetSenha;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel painelLogin;
     private jpass.JRPasswordField senha;
     private javax.swing.JLabel telaInicial;
